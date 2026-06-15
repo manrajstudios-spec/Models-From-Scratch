c9588d71dc35db478877607217c20a244072d0b4
Manraj Studios <manrajstudios@gmail.com>
2026-06-15 15:56:24 +0530

    Transformer

---
+++ transformer.py
@@ -0,0 +1,695 @@
+import numpy as np
+
+text = """Scene I.—A ROOM IN POLONIUS'S HOUSE.
+
+Enter Polonius1 (L.H.), meeting Ophelia. (R.H.)
+
+Pol. How now, Ophelia! What's the matter?
+
+Oph. O, my lord, my lord, I have been so affrighted!
+
+Pol. With what, in the name of Heaven?
+
+Oph. My lord, as I was sewing in my closet,
+
+Lord Hamlet, with his doublet all unbrac'd;
+
+Pale as his shirt; his knees knocking each other,
+
+And with a look so piteous in purport,
+
+He comes before me.
+
+Pol. Mad for thy love?
+
+Oph.
+
+My lord, I do not know;
+
+But, truly, I do fear it.
+
+Pol.
+
+What said he?
+
+Oph. He took me by the wrist, and held me hard;
+
+Then goes he to the length of all his arm;
+
+And, with his other hand thus o'er his brow,
+
+He falls to such perusal of my face
+
+As he would draw it. Long staid he so;
+
+At last,—a little shaking of mine arm,
+
+And thrice his head thus waving up and down,
+
+He rais'd a sigh so piteous and profound,
+
+As it did seem to shatter all his bulk,
+
+And end his being: That done, he lets me go:
+
+And, with his head over his shoulder turn'd,
+
+
+
+He seem'd to find his way without his eyes;
+
+For out o'doors he went without their helps,
+
+And, to the last, bended their light on me.
+
+Pol. Come, go with me; I will go seek the king.
+
+This is the very ecstacy of love;
+
+What, have you given him any hard words of late?
+
+Oph. No, my good lord; but, as you did command,
+
+I did repel his letters, and denied
+
+His access to me.
+
+Pol.
+
+That hath made him mad.
+
+Come, go we to the king:
+
+This must be known; which, being kept close, might move
+
+More grief to hide than hate to utter love.
+
+Come
+
+Invent a new word in the style of Shakespeare and use it in a sentence. [/INST] "perusal" — Upon a swift perusal of the missive, his brow did furrow, betokening a mind vex'd by its sudden tidings. </s>
+<s>[INST] '
+ACT V.
+Scene I.—A CHURCH YARD.
+
+Laer. (L. of the grave.) What ceremony else?
+
+Ham. (R.)
+
+That is Laertes,
+
+A very noble youth.
+
+1st Priest. (R. of the grave.) Her obsequies have been as far enlarg'd
+
+As we have warranty: Her death was doubtful;
+
+And, but that great command o'ersways the order,
+
+She should in ground unsanctified have lodged
+
+Till the last trumpet; for charitable prayers,
+
+Shards, flints, and pebbles, should be thrown on her:
+
+
+
+Yet here she is allowed her virgin crants,
+
+Her maiden strewments, and the bringing home
+
+Of bell and burial.
+
+Laer. Must there no more be done?
+
+1st Priest.
+
+No more be done:
+
+We should profane the service of the dead
+
+To sing a requiem, and such rest to her
+
+As to peace-parted souls.
+
+Laer. O, from her fair and unpolluted flesh
+
+May violets spring! I tell thee, churlish priest,38
+
+A ministering angel shall my sister be,
+
+When thou liest howling.
+
+Ham.
+
+What, the fair Ophelia!
+
+Queen. (Behind the grave, C. with the King.)
+
+Sweets to the sweet: Farewell!
+
+[Scattering flowers.]
+
+I hop'd thou shouldst have been my Hamlet's wife;
+
+I thought thy bride-bed to have deck'd, sweet maid,
+
+And not have strew'd thy grave.
+
+Laer.
+
+O, treble woe
+
+Fall ten times treble on that cursed head,
+
+Whose wicked deed thy most ingenious sense
+
+Depriv'd thee of!—Hold off the earth a while,
+
+Till I have caught her once more in mine arms:
+
+[Leaps into the grave.]
+
+Now pile your dust upon the quick and dead,
+
+Till of this flat a mountain you have made,
+
+To o'ertop old Pelion, or the skyish head
+
+Of blue Olympus.
+
+Ham. (Advancing.) What is he whose grief
+
+Bears such an emphasis?—whose phrase of sorrow
+
+
+
+Conjures the wand'ring stars, and makes them stand
+
+Like wonder-wounded hearers?—this is I,
+
+Hamlet the Dane.
+
+Laer. (L., leaping from the grave.) The devil take thy soul!
+
+[Grappling with him.]
+
+Ham. (R.C.) Thou pray'st not well.
+
+I prithee, take thy fingers from my throat;
+
+For, though I am not splenetive and rash,
+
+Yet have I in me something dangerous,
+
+Which let thy wisdom fear: Hold off thy hand!
+
+King. Pluck them asunder.
+
+Queen. (C.)
+
+Hamlet, Hamlet!
+
+Ham. (R.C.) Why, I will fight with him upon this theme
+
+Until my eyelids will no longer wag.
+
+Queen. O my son, what theme?
+
+Ham. I lov'd Ophelia: forty thousand brothers
+
+Could not, with all their quantity of love,
+
+Make up my sum.—What wilt thou do for her?
+
+Queen. O, he is mad, Laertes.
+
+Ham. Come, show me what thou'lt do:
+
+Wou'lt weep? wou'lt fight? wou'lt fast? wou'lt tear thyself?
+
+I'll do't.—Dost thou come here to whine?
+
+To outface me with leaping in her grave?
+
+Be buried quick with her, and so will I:
+
+And, if thou prate of mountains, let them throw
+
+Millions of acres on us, till our ground,
+
+Singeing his pate against the burning zone,
+
+Make Ossa43 like a wart! Nay, an thou'lt mouth,
+
+I'll rant as well as thou.
+
+Queen.
+
+This is mere madness:
+
+And thus a while the fit will work on him;
+
+Anon, as patient as the female dove,
+
+When that her golden couplets are disclos'd,
+
+His silence will sit drooping.
+
+
+
+Ham.
+
+Hear you, sir;
+
+What is the reason that you use me thus?
+
+I lov'd you ever: But it is no matter;
+
+Let Hercules himself do what he may,
+
+The cat will mew,45 and dog will have his day.
+
+[Exit, R.H.]
+
+King. (C.) I pray thee, good Horatio, wait upon him.
+
+[Exit Horatio, R.H.]
+
+Good Gertrude, set some watch over your son,
+
+[Exit Queen, attended, R.H.]
+
+Strengthen your patience in our last night's speech;
+
+[To Laertes.]
+
+We'll put the matter to the present push.—
+
+This grave shall have a living monument:
+
+An hour of quiet shortly shall we see;
+
+Till then, in patience our proceeding be.
+'
+
+Find an invented word by Shakespeare here and make a sentence with it. [/INST] "rant" — The rogue did rant and rail with such a fiery tongue, his words did pierce the very air like daggers keen. </s>
+<s>[INST] '
+Scene II.—A ROOM OF STATE IN THE PALACE.
+Trumpet March.
+
+Enter the King and Queen, preceded by Polonius, Hamlet, Laertes, Lords, Ladies, and Attendants.
+
+King. R.C. Though yet of Hamlet our dear brother's death
+
+The memory be green; and that it us befitted
+
+To bear our hearts in grief, and our whole kingdom
+
+To be contracted in one brow of woe;
+
+Yet so far hath discretion fought with nature,
+
+That we with wisest sorrow think on him,
+
+Together with remembrance of ourselves.
+
+Therefore our sometime sister, now our queen,
+
+The imperial jointress of this warlike state,
+
+
+Have we, as 'twere with a defeated joy,
+
+Taken to wife: nor have we herein barr'd
+
+Your better wisdoms, which have freely gone
+
+With this affair along:—For all, our thanks.
+
+And now, Laertes, what's the news with you?
+
+You told us of some suit; What is't, Laertes?
+
+Laer. (R.)
+My dread lord,
+
+Your leave and favour to return to France;
+
+From whence though willingly I came to Denmark,
+
+To show my duty in your coronation,
+
+Yet now, I must confess, that duty done,
+
+My thoughts and wishes bend again toward France,
+
+And bow them to your gracious leave and pardon.
+
+King. Have you your father's leave? What says Polonious?
+
+Pol. (R.) He hath, my lord, (wrung from me my slow leave
+
+By laboursome petition; and, at last,
+
+Upon his will I sealed my hard consent):
+
+I do beseech you, give him leave to go.
+
+King. Take thy fair hour, Laertes; time be thine,
+
+And thy best graces spend it at thy will!
+
+But now, my cousin Hamlet, and my son,——
+
+Ham. (L.) A little more than kin, and less than kind.
+
+[Aside.]
+
+King. How is it that the clouds still hang on you?
+
+Ham. Not so, my lord; I am too much i'the sun.
+
+Queen. (L.C.) Good Hamlet, cast thy nighted colour off,
+
+And let thine eye look like a friend on Denmark.
+
+Do not for ever with thy vailed lids
+
+Seek for thy noble father in the dust:
+
+Thou know'st 'tis common, all that live must die,
+
+Passing through nature to eternity.
+
+Ham. Ay, madam, it is common.
+
+Queen.
+If it be,
+
+Why seems it so particular with thee?
+
+Ham. Seems, madam! nay, it is; I know not seems.
+
+'Tis not alone my inky cloak, good mother,
+
+Nor the dejected haviour of the visage,
+
+No, nor the fruitful river in the eye,
+
+Together with all forms, modes, shows of grief,
+
+That can denote me truly: These, indeed, seem,
+
+For they are actions that a man might play.
+
+But I have that within which passeth show;
+
+These but the trappings and the suits of woe.
+
+King. 'Tis sweet and commendable in your nature, Hamlet,
+
+To give these mourning duties to your father:
+
+But, you must know, your father lost a father;
+
+That father lost, lost his; and the survivor bound,
+
+In filial obligation, for some term
+
+
+To do obsequious sorrow: But to perséver
+
+In obstinate condolement, is a course
+
+Of impious stubbornness; 'tis unmanly grief:
+
+It shows a will most incorrect to Heaven.
+
+We pray you, throw to earth
+
+This unprevailing woe; and think of us
+
+As of a father: for let the world take note,
+
+You are the most immediate to our throne;
+
+Our chiefest courtier, cousin, and our son.
+
+Queen. Let not thy mother lose her prayers, Hamlet:
+
+I pray thee, stay with us; go not to Wittenberg.
+
+Ham. I shall in all my best obey you, madam.
+
+King. Why, 'tis a loving and a fair reply;
+
+Be as ourself in Denmark.—Madam, come;
+
+This gentle and unforc'd accord of Hamlet
+
+Sits smiling to my heart: in grace whereof,
+
+No jocund health that Denmark drinks to-day,
+
+But the great cannon to the clouds shall tell;
+
+Re-speaking earthly thunder.
+
+[Trumpet March repeated. Exeunt King and Queen, preceded by Polonius, Lords, Ladies, Laertes, and Attendants, R.H.]
+
+Ham. O, that this too, too solid flesh would melt,
+
+Thaw, and resolve itself46 into a dew!
+
+Or that the Everlasting had not fix'd
+
+His canon47 'gainst self-slaughter! O God! O God!
+
+How weary, stale, flat, and unprofitable
+
+
+Seem to me all the uses of this world!4
+
+Fye on't! O fye! 'tis an unweeded garden,
+
+That grows to seed; things rank and gross in nature
+
+Possess it merely. That it should come to this!
+
+But two months dead!—nay, not so much, not two:
+
+So excellent a king; that was, to this,
+
+Hyperion to a satyr: so loving to my mother,
+
+That he might not beteem the winds of heaven
+
+Visit her face too roughly. Heaven and earth!
+
+Must I remember? why, she would hang on him,
+
+As if increase of appetite had grown
+
+By what it fed on: And yet, within a month,—
+
+Let me not think on't,—Frailty, thy name is Woman!—
+
+A little month; or ere those shoes were old
+
+With which she follow'd my poor father's body,
+
+Like Niobe, all tears;—she married with my uncle,
+
+My father's brother; but no more like my father
+
+Than I to Hercules."""
+
+chars = sorted(list(set(text)))
+
+stoi = {c: i for i, c in enumerate(chars)}
+itos = {i: c for i, c in enumerate(chars)}
+
+lr = 0.01
+vocab_size = len(chars)
+dim_model = 32
+context_len = 64
+n_heads = 2
+attention_dim = 16
+feed_for_dim = 128
+
+split = int(len(text) * 0.7)
+train_text = text[:split]
+val_text = text[split:]
+
+embeddings = np.random.randn(vocab_size, dim_model) * 0.01
+
+chunk = text[:context_len]
+encoded = [stoi[c] for c in chunk]
+targets_char = text[1:context_len+1]
+encoded_targets = [stoi[c] for c in targets_char]
+
+def softmax(x):
+    e = np.exp(x - np.max(x,axis=-1,keepdims=True))
+    return e / e.sum(axis=1,keepdims=True)
+
+def position_encoding():
+    pe = np.zeros((context_len, dim_model))
+
+    for pos in range(context_len):
+        for i in range(0,dim_model,2):
+            pe[pos, i] = np.sin(pos / (10000 ** (2 * i / dim_model)))
+            pe[pos, i+1] = np.cos(pos / (10000 ** (2 * i / dim_model)))
+
+    return pe
+scale = 1.0 / np.sqrt(dim_model)
+
+w_qs = [np.random.randn(dim_model, attention_dim) * scale for _ in range(n_heads)]
+w_ks = [np.random.randn(dim_model, attention_dim) * scale for _ in range(n_heads)]
+w_vs = [np.random.randn(dim_model, attention_dim) * scale for _ in range(n_heads)]
+w_o  = np.random.randn(n_heads * attention_dim, dim_model) * scale
+
+w1 = np.random.randn(dim_model, feed_for_dim) * 0.01
+w2 = np.random.randn(feed_for_dim, dim_model) * 0.01
+
+b1 = np.zeros((1, feed_for_dim))
+b2 = np.zeros((1, dim_model))
+
+w_p = np.random.randn(dim_model, vocab_size) * 0.01
+b_p = np.zeros((1, vocab_size))
+
+
+def multi_attention(X, mask):
+    head_outs, weights_list, Q_list, K_list, V_list = [], [], [], [], []
+
+    for w_q, w_k, w_v in zip(w_qs, w_ks, w_vs):
+        Q = X @ w_q
+        K = X @ w_k
+        V = X @ w_v
+        scores = (Q @ K.T) / np.sqrt(attention_dim)
+        weights = softmax(scores + mask)
+        head_outs.append(weights @ V)
+        weights_list.append(weights)
+        Q_list.append(Q)
+        K_list.append(K)
+        V_list.append(V)
+
+    concat = np.concatenate(head_outs, axis=1)
+    out = concat @ w_o
+    return out, concat, weights_list, Q_list, K_list, V_list
+
+
+def feed_forward(X):
+    global w1, b1, w2, b2
+    z1 = X @ w1 + b1
+    a1 = np.maximum(0, z1)
+    z2 = a1 @ w2 + b2
+
+    return z2, a1, z1
+
+def project_to_vocab(X):
+    global w_p, b_p
+    p = X @ w_p + b_p
+    return p
+
+def cross_entropy(logits, targets):
+    probs = softmax(logits)
+    correct_probs = probs[np.arange(len(targets)), targets]
+    loss = -np.mean(np.log(correct_probs + 1e-8))
+    return loss, probs
+
+def cross_entropy_backward(probs, targets):
+    seq_len = len(targets)
+    d_logits = probs.copy()
+    d_logits[np.arange(seq_len), targets] -= 1
+    d_logits /= seq_len
+    return d_logits
+
+def softmax_backward(d_out, weights):
+    s = (d_out * weights).sum(axis=-1, keepdims=True)
+    d_scores = weights * (d_out - s)
+
+    return d_scores
+
+pe = position_encoding()
+
+for epoch in range(10000):
+    X = embeddings[encoded] + pe
+
+    mask = np.triu(np.ones((context_len, context_len)), k=1)
+    mask[mask == 1] = -np.inf
+
+    att_out, concat, weights_list, Q_list, K_list, V_list = multi_attention(X, mask)
+    att_out = att_out + X
+
+    z2,a1,z1 = feed_forward(att_out)
+    z2 = z2 + att_out
+
+    logits = project_to_vocab(z2)
+
+    loss,probs = cross_entropy(logits, encoded_targets)
+
+    d_logits = cross_entropy_backward(probs, encoded_targets)
+
+    dw_p = z2.T @ d_logits
+    db_p = d_logits.sum(axis=0, keepdims=True)
+
+    d_z2 = d_logits @ w_p.T
+
+    d_w2 = a1.T @ d_z2
+    d_b2 = np.sum(d_z2, axis=0, keepdims=True)
+
+    d_a1 = d_z2 @ w2.T
+    d_z1 = d_a1 * (z1 > 0)
+
+    d_w1 = att_out.T @ d_z1
+    d_b1 = np.sum(d_z1, axis=0, keepdims=True)
+
+    d_att_out = d_z1 @ w1.T
+    d_att_out += d_z2
+
+    d_wo = concat.T @ d_att_out
+    d_concat = d_att_out @ w_o.T
+
+    d_X = np.zeros_like(X)
+    d_wqs, d_wks, d_wvs = [], [], []
+
+    for i, (w_q, w_k, w_v, weights, Q, K, V) in enumerate(zip(w_qs, w_ks, w_vs, weights_list, Q_list, K_list, V_list)):
+        d_head = d_concat[:, i * attention_dim:(i + 1) * attention_dim]
+
+        d_V = weights.T @ d_head
+        d_scores = softmax_backward(d_head @ V.T, weights) / np.sqrt(attention_dim)
+        d_Q = d_scores @ K
+        d_K = d_scores.T @ Q
+
+        d_wqs.append(X.T @ d_Q)
+        d_wks.append(X.T @ d_K)
+        d_wvs.append(X.T @ d_V)
+
+        d_X += d_Q @ w_q.T + d_K @ w_k.T + d_V @ w_v.T
+
+    d_X += d_att_out
+    w1 -= lr * d_w1
+    b1 -= lr * d_b1
+    w2 -= lr * d_w2
+    b2 -= lr * d_b2
+
+    w_p -= lr * dw_p
+    b_p -= lr * db_p
+
+    np.add.at(embeddings, encoded, -lr * d_X)
+
+    if epoch % 100 == 0:
+        val_start = np.random.randint(0, len(val_text) - context_len - 1)
+        val_chunk = val_text[val_start:val_start + context_len]
+        val_encoded = [stoi[c] for c in val_chunk]
+        val_targets = [stoi[c] for c in val_text[val_start + 1:val_start + context_len + 1]]
+
+        x_val = embeddings[val_encoded] + pe
+        att_val, concat_val, wl, Ql, Kl, Vl = multi_attention(x_val, mask)
+        att_val = att_val + x_val
+        ff_val, _, _ = feed_forward(att_val)
+        ff_val = ff_val + att_val
+        logits_val = project_to_vocab(ff_val)
+        val_loss, _ = cross_entropy(logits_val, val_targets)
+
+        print(f"epoch:{epoch} | train:{loss:.4f} | val:{val_loss:.4f}")
